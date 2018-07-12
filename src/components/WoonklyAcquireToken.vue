6ae4ba3ca7d96f1a02f328fcb20a4d4573d3ec24
Jacob Nuño <jacobcnda@gmail.com>
2018-07-12 18:25:19 -0500

    Acquire token component finished

--- src/components/WoonklyAcquireToken.vue
+++ src/components/WoonklyAcquireToken.vue
@@ -1,403 +1,178 @@
 <template>
-    <section id="woonkly-ac-token" class="section">
-
-        <div class="container-tkn">
-            <img src="/img/icons/linea_luz.svg" class="linealuz-tkn">
-            <h3 class="title is-4 has-text-centered">ADQUIRIR TOKENS</h3>
+  <section id="woonkly-ac-token" class="section">
+    <img src="/img/icons/linea_luz.svg" id="blue-line-left">
+    <div class="is-size-4-mobile is-size-2-tablet has-text-centered">ADQUIRIR TOKENS</div>
+      <div class="columns">
+        <div class="column is-12-mobile is-4-tablet is-offset-2-tablet">
+          <p class="has-text-left is-size-7-mobile is-size-6-tablet is-3-tablet w-p">
+              Con el objetivo de premiar la atención de las personas cuando ven vídeos o interactúan con el contenido , hemos creado nuestra criptomoneda, llamada “woonk”. Los creadores de contenido y anunciantes pueden comprar woonks para repartirlos entre las personas que vean, compartan e interactuen con su contenido.
+              Estas son las fases de la Oferta inicial de nuestra criptomoneda:
+          </p>
+          <ul>
+            <li v-for="(list1, listIndex) in list1" :key="listIndex" class="list-tkn">
+              <img src="/img/icons/group.svg" class="bullet">
+              <div>
+                <div class="is-size-6-mobile is-size-4-tablet">{{ list1.title }}</div>
+                {{ list1.content }}
+              </div>
+            </li>
+          </ul>
         </div>
-
-        <div class="columns">
-            <div class="column is-5 cl-a">
-
-                <p class="text-tkn">
-                    Con el objetivo de premiar la atención de las
-                    personas cuando ven vídeos o interactúan con
-                    el contenido , hemos creado nuestra
-                    criptomoneda, llamada “woonk”. Los creadores
-                    de contenido y anunciantes pueden comprar
-                    woonks para repartirlos entre las personas que
-                    vean, compartan e interactuen con su contenido. 
-                    Estas son las fases de la Oferta
-                    inicial de nuestra criptomoneda:
-                </p>
-
-                <br>
-
-                <ul>
-                    <li v-for="(list, listIndex) in lists" :key="listIndex" class="list-tkn">
-                        <img src="/img/icons/group.svg" class="img-tkn">
-                        <div class="des-tkn">
-                            <h1 class="title is-6">{{list.title}}</h1>
-                            {{list.content}}
-                        </div>
-                    </li>
-                </ul>
-
-            </div>
-            
-            <div class="column is-7 cl-b">
-                <ul>
-                    <li v-for="(list, listIndex) in lists_two" :key="listIndex" class="list-tkn">
-                        <img src="/img/icons/group.svg" class="img-tkn">
-                        <div class="des-tkn">
-                            <h1 class="title is-6">{{list.title}}</h1>
-                            {{list.content}}
-                        </div>
-                    </li>
-                </ul>
-            </div>
-                
+        <div class="column is-12-mobile is-6-tablet is-offset-1-tablet w-div-right">
+          <ul>
+            <li v-for="(list2, listIndex) in list2" :key="listIndex" class="list-tkn">
+              <img src="/img/icons/group.svg" class="bullet">
+              <div>
+                <div class="is-size-6-mobile is-size-4-tablet">{{ list2.title }}</div>
+                {{ list2.content }}
+              </div>
+            </li>
+          </ul>
 
         </div>
 
-        
-        
-        <img src="/img/icons/linea_luz.svg" class="linealuzz-tkn">
-        <img src="/img/icons/ilustracion.svg" class="ilustracion-tkn">
+      </div>
+      <img src="/img/icons/linea_luz.svg" id="blue-line-right">
+      <img src="/img/icons/ilustracion.svg" id="blue-cube">
 
     </section>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            lists: [
-                {
-                    title: '1 Mayo-1 Junio:', 
-                    content: `Venta privada para suscriptores de
-                    Mr. Santos en Youtube (50%
-                    Bonus)`
-                },
-                {
-                    title: '1 Junio-1 Julio:',
-                    content: `Venta privada
-                    para suscriptores de otros
-                    canales de Youtube (40%
-                    Bonus)`
-                },
-                {
-                    title:'1 Julio- 1 Septiembre:',
-                    content: `Venta
-                    privada (Capitales de Emiratos
-                    Árabes, Hong Kong y Europa)
-                    (30% Bonus)`
-                },
-                {
-                    title: '1 Septiembre-1 Octubre:',
-                    content: `ICO Fase 1 (20% Bonus)`
-                },
-                {
-                    title: '1 Octubre- 1 Noviembre:',
-                    content: `ICO Fase 2 (10% Bonus)`
-                },
-                {
-                    title: '1 Noviembre- 31 Diciembre:',
-                    content: `ICO Fase 3 (0% Bonus)`
-                }
-                             
-            ],
-
-            lists_two: [
-                {
-                    title: 'Inicio',
-                    content: `1 Mayo 2018`
-                },   
-                {
-                    title: 'Número de Woonks a la venta',
-                    content: `3,000,000,000 (30%)`
-                },
-                {
-                    title: 'Final de la ICO',
-                    content: `31 Diciembre 2018`
-                },
-                {
-                    title: 'Tasa de cambio',
-                    content: `1 ETH = 50,000 Woonks`
-                },
-                {
-                    title: 'Métodos de inversión aceptados',
-                    content: `ETH (BTC, LTC, DASH próximamente)`
-                },
-                {
-                    title: 'Inversión mínima',
-                    content: `0.01 ETH`
-                }
-            ]
+  data () {
+    return{
+      list1: [
+        {
+            title: '1 Mayo-1 Junio:',
+            content: `Venta privada para suscriptores de
+            Mr. Santos en Youtube (50%
+            Bonus)`
+        },
+        {
+            title: '1 Junio-1 Julio:',
+            content: `Venta privada
+            para suscriptores de otros
+            canales de Youtube (40%
+            Bonus)`
+        },
+        {
+            title:'1 Julio- 1 Septiembre:',
+            content: `Venta
+            privada (Capitales de Emiratos
+            Árabes, Hong Kong y Europa)
+            (30% Bonus)`
+        },
+        {
+            title: '1 Septiembre-1 Octubre:',
+            content: `ICO Fase 1 (20% Bonus)`
+        },
+        {
+            title: '1 Octubre- 1 Noviembre:',
+            content: `ICO Fase 2 (10% Bonus)`
+        },
+        {
+            title: '1 Noviembre- 31 Diciembre:',
+            content: `ICO Fase 3 (0% Bonus)`
         }
+      ],
+      list2: [
+        {
+            title: 'Inicio',
+            content: `1 Mayo 2018`
+        },
+        {
+            title: 'Número de Woonks a la venta',
+            content: `3,000,000,000 (30%)`
+        },
+        {
+            title: 'Final de la ICO',
+            content: `31 Diciembre 2018`
+        },
+        {
+            title: 'Tasa de cambio',
+            content: `1 ETH = 50,000 Woonks`
+        },
+        {
+            title: 'Métodos de inversión aceptados',
+            content: `ETH (BTC, LTC, DASH próximamente)`
+        },
+        {
+            title: 'Inversión mínima',
+            content: `0.01 ETH`
+        }
+      ]
     }
+  }
 };
 </script>
 
 <style lang="scss" scope>
 #woonkly-ac-token {
-  background: #0a0830;
+  background: var(--woonkly-black-blue);
   .extended-brief {
     margin-top: 1.5em;
   }
-}
 
+  #blue-line-left {
+    position: absolute;
+    transform: translate(-30%, -45%);
+    width: 6em;
+  }
 
+  #blue-line-right {
+    position: absolute;
+    transform: translate(55%, -170%);
+    width: 14em;
+  }
 
-/*Responsive*/
-/*.grid{
-    display: grid;
-    grid-template-columns: [xl-start] 1fr 1.5rem [md-start] minmax(0, 640px) [md-end] 1.5rem 1fr [xl-end];
-    grid-gap: 5px;
-}
+  #blue-cube {
+    position: absolute;
+    transform: translate(74%, -88%);
+    width: 12em;
+  }
 
-.grid * {grid-column: md;}
-.grid-xl * {grid-column: md;}*/
+  .bullet {
+    max-width: 10%;
+  }
 
-@media (min-width: 320px) and (max-width: 640px) {
-  
-    .list-tkn {
-        display: flex;
-        align-items: center;
-        margin: 20px 20px 0px 20px;
-        text-align: left;
-        position: relative;
-        
-    }
+  .list-tkn {
+    display: flex;
+    align-items: flex-start;
+    margin: 1em;
+    text-align: left;
+    position: relative;
+  }
 
-    .img-tkn{
-        width:30px;
-        top:0px;
-        position: absolute;
-    }
-
-    .text-tkn{
-        font-size: 1em;
-        margin: 20px;
-        text-align: justify;
-    }
-
-    .des-tkn{
-        font-size: 1.1em;
-        margin-left: 30px;
-    }
-
-    .container-tkn{
-        align-items: center;
-        margin-bottom: 50px;
-        margin-top: 50px;
-    }
-
-    .linealuz-tkn{
-        position: absolute;
-        transform: translate(-35%, -50%);
-        z-index: 1;
-        width:40%;
-    }
-
-    .ilustracion-tkn{
-        position: absolute;
-        transform: translate( 130%, -90%);
-        z-index: 2;
-        width:40%;
-    }
-    .linealuzz-tkn{
-        position: absolute;
-        transform: translate(105%, -220%);
-        z-index: 3;
-        width:45%;
-    }
-  
-}
-
-@media (min-width: 641px) and (max-width: 767px) {
-  
-    .list-tkn {
-        display: flex;
-        align-items: center;
-        margin: 20px 20px 0px 20px;
-        text-align: left;
-        position: relative;
-        
-    }
-
-    .img-tkn{
-        width:30px;
-        top:0px;
-        position: absolute;
-    }
-
-    .text-tkn{
-        font-size: 1em;
-        margin: 20px;
-        text-align: justify;
-    }
-
-    .des-tkn{
-        font-size: 1.1em;
-        margin-left: 30px;
-    }
-
-    .container-tkn{
-        align-items: center;
-        margin-bottom: 50px;
-        margin-top: 50px;
-    }
-
-    .linealuz-tkn{
-        position: absolute;
-        transform: translate(-35%, -50%);
-        z-index: 1;
-        width:40%;
-    }
-
-    .ilustracion-tkn{
-        position: absolute;
-        transform: translate( 120%, -90%);
-        z-index: 2;
-        width:45%;
-    }
-    .linealuzz-tkn{
-        position: absolute;
-        transform: translate(105%, -150%);
-        z-index: 3;
-        width:45%;
-    }
-  
-}
-
-@media (min-width: 768px) and (max-width: 1024px) {
-
-    .linealuz-tkn{
-        position: absolute;
-        transform: translate(-20%, -10%);
-        z-index: 1;
-        width:30%
-    }
-
-    .ilustracion-tkn{
-        position: absolute;
-        transform: translate( 175%, -100%);
-        z-index: 2;
-        width:40%;
-    }
-    .linealuzz-tkn{
-        position: absolute;
-        transform: translate(205%, -80%);
-        z-index: 3;
-        width:20%;
-    }
-
-    .text-tkn{
-        font-size: 1em;
-        //margin: 60px 60px 20px 140px;
-        margin-left: 30px;
-        text-align: justify;
-        //padding: 30px 50px 30px 50px;
-    }
-
-    .list-tkn {
-        display: flex;
-        align-items: center;
-        //margin: 10px 60px 20px;
-        margin: 5px;
-        //padding: 30px 10px 30px 10px;
-        text-align: left;
-        position: relative;
-    }
-
-    .img-tkn{
-        width:30px;
-        top:0px;
-        position: absolute;
-    }
-
-    .des-tkn{
-        font-size: 1em;
-        margin-left: 30px;
-    }
-
-    .cl-a{
-        margin: 50px 5px 0px 100px;
-    }
-    .cl-b{
-        margin: 70px 0px 0px 0px;
-    }
-
-    .title{
-        font-size: 4em;
-    }
-
-    /*.cont{
-        -webkit-column-count: 2; 
-        -moz-column-count: 2; 
-        column-count: 2;
-        margin: 50px 50px 0px 80px;
-        //column-gap: 50px 100px 50px 300px;
-    }*/
+  .w-p {
+    margin: 1em 0 4em;
+  }
 
 }
 
-@media (min-width: 1025px) {
-  
-   .linealuz-tkn{
-        position: absolute;
-        transform: translate(-20%, -10%);
-        z-index: 1;
-        width:20%
+@media only screen and (min-width: 769px) {
+  #woonkly-ac-token {
+    .bullet {
+      max-width: 5%;
+    }
+    .w-div-right {
+      margin-top: 4em;
+    }
+    #blue-line-left {
+      transform: translate(12%, -13%);
+      width: 16em;
     }
 
-    .ilustracion-tkn{
-        position: absolute;
-        transform: translate( 175%, -100%);
-        z-index: 2;
-        width:35%;
-    }
-    .linealuzz-tkn{
-        position: absolute;
-        transform: translate(190%, -60%);
-        z-index: 3;
-        width:20%;
+    #blue-line-right {
+      transform: translate(290%, -90%);
+      width: 16em;
     }
 
-    .text-tkn{
-        font-size: 1.2em;
-        //margin: 60px 60px 20px 140px;
-        margin-left: 30px;
-        text-align: justify;
-        //padding: 30px 50px 30px 50px;
+    #blue-cube {
+      transform: translate(220%, -105%);
+      width: 30em;
     }
-
-    .list-tkn {
-        display: flex;
-        align-items: center;
-        //margin: 10px 60px 20px;
-        margin: 5px;
-        //padding: 30px 10px 30px 10px;
-        text-align: left;
-        position: relative;
-    }
-
-    .img-tkn{
-        width:30px;
-        top:0px;
-        position: absolute;
-    }
-
-    .des-tkn{
-        font-size: 1.1em;
-        margin-left: 30px;
-        margin-bottom: 10px;
-    }
-
-    .cl-a{
-        margin: 50px 5px 0px 100px;
-    }
-    .cl-b{
-        margin: 70px 0px 0px 0px;
-    }
-
-    .title{
-        font-size: 4em;
-    }
-  
-  
+  }
 }
 
 </style>
-
--- src/components/WoonklyDistributionToken.vue
+++ src/components/WoonklyDistributionToken.vue
@@ -1,13 +1,6 @@
 <template>
-
- <section id="woonkly-dis-token" class="section columns">
-     
-
+  <section id="woonkly-dis-token" class="section columns">
      <div class="graf-tkn column is-6">
-
- <section id="woonkly-dis-token" class="section">
-     <div class="graf-tkn">
-
         <h3 class="title is-4 has-text-centered">Distribución de tokens</h3>
 
         <a class="des-dtkn atkn">
@@ -41,7 +34,7 @@
 
         <img src="/img/sections/grafica_tkn.svg" class="graf-one">
      </div>
-    
+
      <div class="graf-tkn column is-6">
         <h3 class="title is-4 has-text-centered">Uso de fondos</h3>
 
@@ -68,283 +61,86 @@
 
 
         <img src="/img/sections/grafica_tkn2.svg" class="graf-two">
-     </div>
-
-
-</section>
-    
-
+    </div>
+   </section>
 </template>
 
 <style lang="scss">
 #woonkly-dis-token {
   background: #0a0830;
-  
+
   .extended-brief {
     margin-top: 1.5em;
   }
 
-
-.graf-tkn {
-    position: relative;
-    align-items: center;
-    text-align: center;
-    }
-
-@media (min-width: 320px) and (max-width: 640px) {
-    
-    .graf-one{
-        width:100%;
-        max-width: 500%;
-
-    }
-    .graf-two{
-        width:100%;
-        max-width: 500%;
-
-    }
-
-    .des-dtkn{
-        font-size: 0.8em;
-        position: absolute;
-        z-index: 1;
-    }
-
-    .atkn{
-        transform: translate(75%, 10%);
-    }
-
-    .btkn{
-        transform: translate(400%, 40%);
-    }
-
-    .ctkn{
-        transform: translate(750%, 320%);
-    }
-
-    .dtkn{
-        transform: translate(500%, 550%);
-    }
-    .etkn{
-        transform: translate(70%, 700%);
-    }
-    .ftkn{
-        transform: translate(60%, 480%);
-    }
-    .gtkn{
-        transform: translate(-10%, 180%);
-    }
-    .htkn{
-        transform: translate(70%, 50%);
-    }
-    .itkn{
-        transform: translate(380%, 25%);
-    }
-    .jtkn{
-        transform: translate(260%,500%);
-    }
-    .ktkn{
-        transform: translate(135%,680%);
-    }
-    .ltkn{
-        transform: translate(10%,280%);
-    }
-}
-
-@media (min-width: 641px) and (max-width: 767px) {
-
-    .graf-one{
-        width:100%;
-        max-width: 500%;
-
-    }
-    .graf-two{
-        width:100%;
-        max-width: 500%;
-
-    }
-
-    .des-dtkn{
-        font-size: 0.8em;
-        position: absolute;
-        z-index: 1;
-    }
-
-    .atkn{
-        transform: translate(115%, 50%);
-    }
-
-    .btkn{
-        transform: translate(600%, 70%);
-    }
-
-    .ctkn{
-        transform: translate(1020%, 500%);
-    }
-
-    .dtkn{
-        transform: translate(700%, 750%);
-    }
-    .etkn{
-        transform: translate(110%, 950%);
-    }
-    .ftkn{
-        transform: translate(100%, 680%);
-    }
-    .gtkn{
-        transform: translate(20%, 200%);
-    }
-    .htkn{
-        transform: translate(100%, 110%);
-    }
-    .itkn{
-        transform: translate(480%, 75%);
-    }
-    .jtkn{
-        transform: translate(380%,700%);
-    }
-    .ktkn{
-        transform: translate(200%,920%);
-    }
-    .ltkn{
-        transform: translate(30%,390%);
-    }
-
-}
-
-@media (min-width: 768px) and (max-width: 1024px) {
-
-    .graf-one{
-        width:100%;
-        max-width: 500%;
-
-    }
-    .graf-two{
-        width:100%;
-        max-width: 500%;
-
-    }
-
-    .des-dtkn{
-        font-size: 0.8em;
-        position: absolute;
-        z-index: 1;
-    }
-
-    .atkn{
-        transform: translate(115%, 50%);
-    }
-
-    .btkn{
-        transform: translate(600%, 70%);
-    }
-
-    .ctkn{
-        transform: translate(1020%, 500%);
-    }
-
-    .dtkn{
-        transform: translate(700%, 750%);
-    }
-    .etkn{
-        transform: translate(110%, 950%);
-    }
-    .ftkn{
-        transform: translate(100%, 680%);
-    }
-    .gtkn{
-        transform: translate(20%, 200%);
-    }
-    .htkn{
-        transform: translate(100%, 110%);
-    }
-    .itkn{
-        transform: translate(480%, 75%);
-    }
-    .jtkn{
-        transform: translate(380%,700%);
-    }
-    .ktkn{
-        transform: translate(200%,920%);
-    }
-    .ltkn{
-        transform: translate(30%,390%);
-    }
-
-}
-
-@media (min-width: 1025px) {
-
-    .graf-one{
-        width:100%;
-        max-width: 500%;
-
-    }
-    .graf-two{
-        width:100%;
-        max-width: 500%;
-
-    }
-
-    .des-dtkn{
-        font-size: 1.1em;
-        position: absolute;
-        z-index: 1;
-    }
-
-    .atkn{
-        transform: translate(145%, 80%);
-    }
-
-    .btkn{
-        transform: translate(680%, 120%);
-    }
-
-    .ctkn{
-        transform: translate(1100%, 500%);
-    }
-
-    .dtkn{
-        transform: translate(750%, 800%);
-    }
-    .etkn{
-        transform: translate(125%, 1000%);
-    }
-    .ftkn{
-        transform: translate(110%, 720%);
-    }
-    .gtkn{
-        transform: translate(30%, 230%);
-    }
-    .htkn{
-        transform: translate(130%, 120%);
-    }
-    .itkn{
-        transform: translate(480%, 75%);
-    }
-    .jtkn{
-        transform: translate(380%,780%);
-    }
-    .ktkn{
-        transform: translate(220%,990%);
-    }
-    .ltkn{
-        transform: translate(25%,390%);
-    }
-
-}
-
-
-
-small{
-    display: block;
-}
-
   .graf-tkn {
-    display: absolute;
+    position: relative;
     align-items: center;
     text-align: center;
   }
 }
-</style>
 
+@media only screen and (min-width: 769px) {
+
+    .graf-one{
+        width:100%;
+        max-width: 500%;
+
+    }
+    .graf-two{
+        width:100%;
+        max-width: 500%;
+
+    }
+
+    .des-dtkn{
+        font-size: 0.8em;
+        position: absolute;
+        z-index: 1;
+    }
+
+    .atkn{
+        transform: translate(115%, 50%);
+    }
+
+    .btkn{
+        transform: translate(600%, 70%);
+    }
+
+    .ctkn{
+        transform: translate(1020%, 500%);
+    }
+
+    .dtkn{
+        transform: translate(700%, 750%);
+    }
+    .etkn{
+        transform: translate(110%, 950%);
+    }
+    .ftkn{
+        transform: translate(100%, 680%);
+    }
+    .gtkn{
+        transform: translate(20%, 200%);
+    }
+    .htkn{
+        transform: translate(100%, 110%);
+    }
+    .itkn{
+        transform: translate(480%, 75%);
+    }
+    .jtkn{
+        transform: translate(380%,700%);
+    }
+    .ktkn{
+        transform: translate(200%,920%);
+    }
+    .ltkn{
+        transform: translate(30%,390%);
+    }
+
+}
+small{
+    display: block;
+}
+</style>
