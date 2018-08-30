a516ac8af7addc2f1f45d5541125f811f58c481c
Isaias <soote1.ir@gmail.com>
2018-08-30 16:03:03 -0500

    WoonklyButton hover effect backed up

--- src/components/shared/WoonklyButton.vue
+++ src/components/shared/WoonklyButton.vue
@@ -1,201 +1,149 @@
 <template>
-  <a v-if="href" :href="href" target="_self" v-bind:class="chooseClass">
-    <span class="inner-border">
-      <slot/>
-    </span>
-  </a>
-  <button v-else v-bind:class="chooseClass">
-    <span class="inner-border">
-      <slot/>
-    </span>
-  </button>
+ <a v-if="href" :href="href" target="_self" v-bind:class="chooseClass">
+   <span class="inner-border">
+     <slot/>
+   </span>
+ </a>
+ <button v-else v-bind:class="chooseClass">
+   <span class="inner-border">
+     <slot/>
+   </span>
+ </button>
 </template>
 
 <script>
 export default {
-  props: {
-    href: {
-      default: null
-    },
-    buttonClass: ""
-  },
-  computed: {
-    chooseClass: function() {
-      switch (this.buttonClass) {
-        case "woonkly-button-light-to-dark": {
-        }
-        case "woonkly-button-opacity-to-light": {
-        }
-        case "woonkly-button": {
-          return this.buttonClass;
-        }
-        default: {
-          return "woonkly-button";
-        }
-      }
-    }
-  }
+ props: {
+   href: {
+     default: null
+   },
+   buttonClass: ""
+ },
+ computed: {
+   chooseClass: function() {
+     switch (this.buttonClass) {
+       case "woonkly-button-light-to-dark": {
+       }
+       case "woonkly-button-opacity-to-light": {
+       }
+       case "woonkly-button": {
+         return this.buttonClass;
+       }
+       default: {
+         return "woonkly-button";
+       }
+     }
+   }
+ }
 };
 </script>
 
 
 <style lang="scss">
 .woonkly-button {
-  border: none;
-  background: none;
-  color: var(--woonkly-light-blue) !important;
-  padding: 0.25em;
-  border: 0.8px var(--woonkly-light-blue-quarter-opacity) solid;
-  cursor: pointer;
-  border-radius: 3em;
-  font-size: 12.5px;
-  
-  &:focus,
-  &:hover {
-    text-decoration: none;
-  }
-  
-  .inner-border {
-    border-radius: inherit;
-    padding: 8.5px;
-    display: block;
-    border: 1.2px var(--woonkly-light-blue) solid;
-  }
+ border: none;
+ background: none;
+ color: var(--woonkly-light-blue) !important;
+ padding: 0.25em;
+ border: 0.8px var(--woonkly-light-blue-quarter-opacity) solid;
+ cursor: pointer;
+ border-radius: 3em;
+ font-size: 12.5px;
 
-  @media screen and (max-width: 1104px) {
-    font-size: 10px;
-  }
+ .inner-border {
+   border-radius: inherit;
+   padding: 8.5px;
+   display: block;
+   border: 1.2px var(--woonkly-light-blue) solid;
+   font-size: 1.19em;
+ }
+
+ &:focus,
+ &:hover {
+   text-decoration: none;
+   -webkit-box-shadow: 0px 0px 85px 4px #08c2ee;
+   box-shadow: 0px 0px 40px 4px var(--woonkly-light-blue-quarter-opacity);
+   color: white !important;
+
+   .inner-border {
+     background: var(--woonkly-gradient-dark-opacity-to-light-right);
+   }
+ }
+
+ @media screen and (max-width: 1104px) {
+   font-size: 10px;
+ }
 }
 .woonkly-button-opacity-to-light {
-  border: none;
-  color: white !important;
-  padding: 0.25em;
-  border: 0.8px var(--woonkly-light-blue-quarter-opacity) solid;
-  cursor: pointer;
-  border-radius: 3em;
-  font-size: 12.5px;
-  text-decoration: none;
-  background: none;
+ border: none;
+ color: white !important;
+ padding: 0.25em;
+ border: 0.8px var(--woonkly-light-blue-quarter-opacity) solid;
+ cursor: pointer;
+ border-radius: 3em;
+ font-size: 12.5px;
+ text-decoration: none;
+ background: none;
 
-  .inner-border {
-    background: none;
-    background: var(--woonkly-gradient-opacity-to-light-right);
-    border-radius: inherit;
-    padding: 8.5px;
-    display: block;
-    font-size: 1.19em;
-    //text-decoration: none;
-  }
+ .inner-border {
+   background: none;
+   background: var(--woonkly-gradient-opacity-to-light-right);
+   border-radius: inherit;
+   padding: 8.5px;
+   display: block;
+   font-size: 1.19em;
+ }
 
-  @media screen and (max-width: 1104px) {
-    font-size: 10px;
-  }
+ &:focus,
+ &:hover {
+   text-decoration: none;
+   -webkit-box-shadow: 0px 0px 85px 4px #08c2ee;
+   box-shadow: 0px 0px 40px 4px var(--woonkly-light-blue-quarter-opacity);
+   color: white !important;
+
+   .inner-border {
+     background: var(--woonkly-light-blue);
+   }
+ }
+
+ @media screen and (max-width: 1104px) {
+   font-size: 10px;
+ }
 }
 .woonkly-button-light-to-dark {
-  border: none;
-  color: white !important;
-  padding: 0.25em;
-  border: 0.8px var(--woonkly-light-blue-quarter-opacity) solid;
-  cursor: pointer;
-  border-radius: 3em;
-  font-size: 12.5px;
-  text-decoration: none;
-  background: none;
+ border: none;
+ color: white !important;
+ padding: 0.25em;
+ border: 0.8px var(--woonkly-light-blue-quarter-opacity) solid;
+ cursor: pointer;
+ border-radius: 3em;
+ font-size: 12.5px;
+ text-decoration: none;
+ background: none;
 
-  .inner-border {
-    background: none;
-    background: var(--woonkly-gradient-light-to-dark-right);
-    border-radius: inherit;
-    padding: 8.5px;
-    display: block;
-    font-size: 1.19em;
-    //text-decoration: none;
-  }
+ .inner-border {
+   background: none;
+   background: var(--woonkly-gradient-light-to-dark-right);
+   border-radius: inherit;
+   padding: 8.5px;
+   display: block;
+   font-size: 1.19em;
+ }
 
-  &:focus,
-  &:hover {
-    text-decoration: none;
-    -webkit-box-shadow: 0px 0px 85px 4px #08c2ee;
-    box-shadow: 0px 0px 40px 4px var(--woonkly-light-blue-quarter-opacity);
-    color: white !important;
+ &:focus,
+ &:hover {
+   text-decoration: none;
+   -webkit-box-shadow: 0px 0px 85px 4px #08c2ee;
+   box-shadow: 0px 0px 40px 4px var(--woonkly-light-blue-quarter-opacity);
+   .inner-border {
+     background: var(--woonkly-light-blue);
+   }
+ }
 
-    .inner-border {
-      background: var(--woonkly-gradient-dark-opacity-to-light-right);
-    }
-  }
-
-  @media screen and (max-width: 1104px) {
-    font-size: 10px;
-  }
-}
-.woonkly-button-opacity-to-light {
-  border: none;
-  color: white !important;
-  padding: 0.25em;
-  border: 0.8px var(--woonkly-light-blue-quarter-opacity) solid;
-  cursor: pointer;
-  border-radius: 3em;
-  font-size: 12.5px;
-  text-decoration: none;
-  background: none;
-
-  .inner-border {
-    background: none;
-    background: var(--woonkly-gradient-opacity-to-light-right);
-    border-radius: inherit;
-    padding: 8.5px;
-    display: block;
-    font-size: 1.19em;
-  }
-
-  &:focus,
-  &:hover {
-    text-decoration: none;
-    -webkit-box-shadow: 0px 0px 85px 4px #08c2ee;
-    box-shadow: 0px 0px 40px 4px var(--woonkly-light-blue-quarter-opacity);
-    color: white !important;
-
-    .inner-border {
-      background: var(--woonkly-light-blue);
-    }
-  }
-
-  @media screen and (max-width: 1104px) {
-    font-size: 10px;
-  }
-}
-.woonkly-button-light-to-dark {
-  border: none;
-  color: white !important;
-  padding: 0.25em;
-  border: 0.8px var(--woonkly-light-blue-quarter-opacity) solid;
-  cursor: pointer;
-  border-radius: 3em;
-  font-size: 12.5px;
-  text-decoration: none;
-  background: none;
-
-  .inner-border {
-    background: none;
-    background: var(--woonkly-gradient-light-to-dark-right);
-    border-radius: inherit;
-    padding: 8.5px;
-    display: block;
-    font-size: 1.19em;
-  }
-
-  &:focus,
-  &:hover {
-    text-decoration: none;
-    -webkit-box-shadow: 0px 0px 85px 4px #08c2ee;
-    box-shadow: 0px 0px 40px 4px var(--woonkly-light-blue-quarter-opacity);
-    .inner-border {
-      background: var(--woonkly-light-blue);
-    }
-  }
-
-  @media screen and (max-width: 1104px) {
-    font-size: 10px;
-  }
+ @media screen and (max-width: 1104px) {
+   font-size: 10px;
+ }
 }
 </style>
+
+
