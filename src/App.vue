d4a5a79e1fecb13b13c41ce5e45d15cf5d895521
Ilich Arredondo <ilich1@hotmail.com>
2018-09-11 21:07:07 -0500

    Moved footer from contact component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,17 @@
     <w-header :current-section="currentSection"/>
     <router-view @userEnteredSection="currentSection = $event"/>
     <w-cookies-banner/>
+    <w-footer/>
   </main>
 </template>
 
 <script>
 // Parallax library
+import wFooter from '@/components/WoonklyFooter'
 import Rellax from 'rellax'
 import wHeader from '@/Header'
 import wCookiesBanner from '@/components/shared/WoonklyCookieBanner'
+import { loadLanguageAsync } from '@/lang'
 
 export default {
   name: 'App',
@@ -21,7 +24,8 @@ export default {
   },
   components: {
     wHeader,
-    wCookiesBanner
+    wCookiesBanner,
+    wFooter
   },
   mounted () {
     if (window.matchMedia('screen and (min-width: 769px)').matches) {
