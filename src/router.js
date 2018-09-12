8140c1b0fe89b46e26c5947d71f40280b93549d8
Ilich Arredondo <ilich1@hotmail.com>
2018-09-11 21:07:43 -0500

    Created beforeEach hook to validate language and added support for URL encoded language

--- src/router.js
+++ src/router.js
@@ -5,28 +5,38 @@ import About from './views/About.vue'
 import PrivacyPolicy from './components/WoonklyPrivacyPolicy.vue'
 import Cookies from './components/WoonklyCookies.vue'
 import NotFound from './components/NotFound404.vue'
+// Change lang function
+import { loadLanguageAsync } from '@/lang'
 
 Vue.use(Router)
 
-export default new Router({
+var determineLanguage = function (lang) {
+  if (lang.includes('en')) {
+    return { simple: 'en', i18n: 'en_UK' }
+  } else if(lang.includes('es')) {
+    return { simple: 'es', i18n: 'es_MX' }
+  } else return { simple: 'en', i18n: 'en_UK' }
+}
+
+const router = new Router({
   routes: [
     {
-      path: '/',
+      path: '/:lang/',
       name: 'home',
       component: Home
     },
     {
-      path: '/about',
+      path: '/:lang/about',
       name: 'about',
       component: About
     },
     {
-      path: '/privacy-policy',
+      path: '/:lang/privacy-policy',
       name: 'privacy policy',
       component: PrivacyPolicy
     },
     {
-      path: '/cookies',
+      path: '/:lang/cookies',
       name: 'cookies',
       component: Cookies
     },
@@ -37,3 +47,42 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if ( to.params.lang == 'es' ) {
+    loadLanguageAsync('es_MX')
+  } else if ( to.params.lang == 'en') {
+    loadLanguageAsync('en_UK')
+  } else {
+    // First we check if local storage is available
+    if (typeof(Storage) !== 'undefined') {
+      // If the user already visited woonkly.com
+      // the localStorage is going to be already set
+      if (localStorage.getItem('woonkly-lang') !== null) {
+        // Finally, if the route param is not
+        // defined we set our own
+        if (to.params.lang) {
+          // If the route param is set we do nothing
+          next()
+          return false
+        }
+        // If the route param is not set get set the one
+        // from our local storage
+        var woonklyLang = localStorage.getItem('woonkly-lang')
+        var wnkLang = determineLanguage(woonklyLang)
+        next({ path: `/${wnkLang.simple}${to.fullPath}` })
+        return false
+      }
+    }
+
+    var userLang = window.navigator.language || navigator.browserLanguage
+    var woonklyLang = determineLanguage(userLang)
+    next({ path: `/${woonklyLang.simple}${to.fullPath}` })
+    
+    if (typeof(Storage) === 'undefined') return false
+    localStorage.setItem('woonkly-lang', woonklyLang.simple)
+  }
+  next()
+})
+
+export default router
